57a6951cb13d01ed9ec6af17792eac85be02703c
Ben Brehaut <bencbrehaut@gmail.com>
2017-01-06 23:26:16 +0000

    Added front-page template file to theme

---
+++ front-page.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The main template file for the front-page.
+ *
+ * Template Name: Front Page
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage halos
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header();
+?>
+
+<?php
+	if ( have_posts() ) :
+
+		/* Start the Loop */
+		while ( have_posts() ) : the_post();
+
+		    get_template_part('template-parts/post');
+
+		endwhile;
+
+	endif;
+?>
+
+<?php get_footer();
