79d5adc0d81dc61aff8f616ace3c705158cb101d
Ben Brehaut <bencbrehaut@gmail.com>
2017-01-18 22:54:30 +0000

    Added IE 9 and lower notice to the header.php file

--- header.php
+++ header.php
@@ -26,6 +26,14 @@
 </head>
 <body <?php body_class(); ?>>
 
+<!-- IE 9 < warning -->
+<!--[if lte IE 9]>
+	<div class="site-notice">
+		<p>You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+	</div>
+<![endif]-->
+<!-- /IE 9 < warning -->
+
 <!-- site-header -->
 <header id="masthead" class="site-header" role="banner">
     <div class="site-title">
