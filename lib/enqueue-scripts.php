2adb62cdf89fead4248c142d290dc9018f05e496
Ben Brehaut <bencbrehaut@gmail.com>
2017-01-30 22:34:33 +0000

    Adding automatic version numbering to style.css and scripts.js file, so that each save changes the version number of the file. To stop caching.

--- lib/enqueue-scripts.php
+++ lib/enqueue-scripts.php
@@ -13,7 +13,10 @@
 
 function theme_scripts() {
     // Enqueue the main Stylesheet.
-    wp_enqueue_style( 'main-stylesheet', get_template_directory_uri() . '/assets/css/style.css', array(), '1.0.0', 'all' );
+
+    $theme_css = get_template_directory() . '/assets/css/style.css';
+
+    wp_enqueue_style( 'main-stylesheet', get_template_directory_uri() . '/assets/css/style.css', array(), filemtime( $theme_css ), 'all' );
 
     // Deregister the jquery version bundled with WordPress, as it does get old.
     wp_deregister_script( 'jquery' );
@@ -23,7 +26,10 @@ function theme_scripts() {
 
     // Enqueue the main javascript file.
     // By default its the uncompressed version, but you can easily change this
-    wp_enqueue_script( 'main-scripts', get_template_directory_uri() . '/assets/js/scripts.js', array('jquery'), '1.0.0', true );
+
+    $theme_js = get_template_directory() . '/assets/js/scripts.js';
+
+    wp_enqueue_script( 'main-scripts', get_template_directory_uri() . '/assets/js/scripts.js', array('jquery'), filemtime( $theme_js ), true );
 
     // Add the comment-reply library on pages where it is necessary
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
