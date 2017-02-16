e7890c0810613a7beca372943e49cf0bf53305fd
Ben Brehaut <bencbrehaut@gmail.com>
2017-02-16 22:24:16 +0000

    Added touch detection to theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,5 +6,18 @@ $(function() {
     */
     console.info( "DOM ready" );
 
+    /**
+    * Touch Device Detection
+    **/
+    var isTouchDevice = 'ontouchstart' in document.documentElement;
+    var html = $('html');
+
+    if ( isTouchDevice ) {
+        html.addClass('touch');
+    }
+    else {
+        html.addClass('no-touch');
+    }
+
     // End jQuery
 });
