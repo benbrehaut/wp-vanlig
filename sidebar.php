21e47b8872717ad8f840507b3c0ffb867d306a4e
Ben Brehaut <bencbrehaut@gmail.com>
2017-03-18 12:46:10 +0000

    Added for each loops for sidebar items
    Adjusted the sidebar funtions so that we can adjust the output of each one. wp_get_archives does not appear to the ability to do this.

--- sidebar.php
+++ sidebar.php
@@ -15,16 +15,17 @@
 
 <aside class="sidebar" role="complementary">
   <!-- widgets -->
-    <?php dynamic_sidebar( 'sidebar-1' ); ?>
+    <?php // dynamic_sidebar( 'sidebar-1' ); ?>
   <!-- widgets -->
 
   <!-- recent posts -->
+  <h4 class="heading-4">Recent Pots</h4>
   <ul class="sidebar-posts">
     <?php
       $recent_posts = wp_get_recent_posts(array( 'orderby' => 'post_date', 'numberposts' => 4, ) );
       foreach( $recent_posts as $recent ) { ?>
-        <li class="sidebar-posts__article">
-          <a href="<?php echo get_permalink($recent["ID"]) ?>"><?php echo $recent["post_title"] ?></a>
+        <li class="sidebar-posts__item">
+          <a class="sidebar-post__link" href="<?php echo get_permalink($recent["ID"]) ?>"><?php echo $recent["post_title"] ?></a>
         </li>
       <?php }
       wp_reset_query();
@@ -33,24 +34,27 @@
   <!-- /recent posts -->
 
   <!-- categories -->
+  <h4 class="heading-4">Categories</h4>
   <ul class="sidebar-categories">
     <?php
-      wp_list_categories( array(
-        'orderby'    => 'name',
-        'title_li'   => '',
-        'show_count' => true,  // show post count
-      ) );
+      $post_categories = get_categories(array ('orderby' => 'name', 'title_li' => '', ));
+      foreach ($post_categories as $post_category) { ?>
+        <li class="sidebar-categories__item">
+          <a class="sidebar-categories__link" href="<?php $post_category->slug; ?>"><?php echo $post_category->name; ?> (<?php echo $post_category->count; ?>)</a>
+        </li>
+      <?php }
     ?>
   </ul>
   <!-- /categories -->
 
   <!-- archives -->
-    <ul class="sidebar-archives">
-      <?php wp_get_archives(array(
-        'orderby'    => 'name',
-        'type'       => 'monthly',
-        'show_count' => true,  // show post count
-      ) ); ?>
-    </ul>
+  <h4 class="heading-4">Archives</h4>
+  <ul class="sidebar-archives">
+    <?php wp_get_archives(array(
+      'orderby'    => 'name',
+      'type'       => 'monthly',
+      'show_count' => true,  // show post count
+    ) ); ?>
+  </ul>
   <!-- /archives -->
 </aside>
