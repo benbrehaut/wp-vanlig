95a130c1b352fba773c7455b436f8537a45f3cff
Ben Brehaut <bencbrehaut@gmail.com>
2017-03-19 23:03:02 +0000

    Added sharing links for posts
    This closes #10

--- single.php
+++ single.php
@@ -13,12 +13,20 @@ get_header();
 <?php while ( have_posts() ) : the_post(); ?>
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         <header class="post-header">
-            <?php the_title(); ?>
+            <?php the_title('<h1 class="heading-1">','</h1>'); ?>
+            <h4 class="heading-4">Post by: <?php the_author(); ?></h4>
         </header>
         <div class="post-content">
             <?php the_content(); ?>
         </div>
+
+        <!-- Comments -->
         <?php comments_template(); ?>
+        <!-- Comments -->
+
+        <!-- Sharing -->
+        <?php get_template_part('template-parts/module-sharing'); ?>
+        <!-- /Sharing -->
     </article>
 <?php endwhile;?>
 
