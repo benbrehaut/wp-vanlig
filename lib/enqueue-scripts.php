1e2e9c897a71b1868cf013c2aa9ce838384db929
Ben Brehaut <bencbrehaut@gmail.com>
2017-03-19 21:54:48 +0000

    ..

--- lib/enqueue-scripts.php
+++ lib/enqueue-scripts.php
@@ -42,9 +42,9 @@ add_action( 'wp_enqueue_scripts', 'theme_scripts' );
 * Add html5shim to header
 **/
 function add_ie_html5_shim () {
-    echo '<!--[if lt IE 9]>';
-    echo '<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>';
-    echo '<![endif]-->';
+  echo '<!--[if lt IE 9]>';
+  echo '<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>';
+  echo '<![endif]-->';
 }
 add_action('wp_head', 'add_ie_html5_shim');
 
