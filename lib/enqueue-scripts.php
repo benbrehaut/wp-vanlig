7994cf1f542ab8e8d1205e5aa09fff63aa9de993
Ben Brehaut <bencbrehaut@gmail.com>
2017-03-19 23:23:33 +0000

    Added action for GA analytics tag
    This closes #8

--- lib/enqueue-scripts.php
+++ lib/enqueue-scripts.php
@@ -48,5 +48,21 @@ function add_ie_html5_shim () {
 }
 add_action('wp_head', 'add_ie_html5_shim');
 
+/**
+* Add Google Analyrics to head
+**/
+function google_analytics() { ?>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-XXXXXXXX-X', 'auto'); // Change me
+    ga('send', 'pageview');
+
+  </script>
+<?php }
+add_action( 'wp_head', 'google_analytics', 10 );
 
 ?>
