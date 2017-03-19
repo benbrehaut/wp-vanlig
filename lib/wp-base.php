60ec2759a9357d8c263ca41bda4a4103aca09bf9
Ben Brehaut <bencbrehaut@gmail.com>
2017-03-19 09:14:48 +0000

    Added support for post formats
    - Added support for all post formats. This closes #4.
    - Adjusted comments in wp-base.php so that @link comment would highlight correctly.

--- lib/wp-base.php
+++ lib/wp-base.php
@@ -16,54 +16,61 @@
 * as indicating support for post thumbnails.
 */
 function wp_base_setup() {
-  /*
+  /**
   * Let WordPress manage the document title.
   * By adding theme support, we declare that this theme does not use a
   * hard-coded <title> tag in the document head, and expect WordPress to
   * provide it for us.
-  */
+  **/
   add_theme_support( 'title-tag' );
 
-  /*
+  /**
   * Menu Support
-  */
+  **/
   add_theme_support( 'menus' );
 
-  /*
+  /**
   * Declare WooCommerce support
-  * http://docs.woothemes.com/document/third-party-custom-theme-compatibility/
-  */
+  * @link http://docs.woothemes.com/document/third-party-custom-theme-compatibility/
+  **/
   add_theme_support( 'woocommerce' );
 
-  /*
+  /**
   * Enable support for Post Thumbnails on posts and pages.
   *
   * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-  */
+  **/
   add_theme_support( 'post-thumbnails' );
 
-  /*
-  * RSS thing..
-  */
+  /**
+  * RSS Feed
+  **/
   add_theme_support( 'automatic-feed-links' );
 
-  /*
+  /**
   * Enable support for Post Formats
   * This has been commented out as it is not used for many projects
   *
-  * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-  */
+  * @link https://developer.wordpress.org/themes/functionality/post-formats/
+  **/
   /*
   add_theme_support( 'post-formats', array(
       'aside',
-      'gallery'
+      'gallery',
+      'link',
+      'image',
+      'quote',
+      'status',
+      'video',
+      'audio',
+      'chat'
   ) );
   */
 
-  /*
+  /**
   * Switch default core markup for search form, comment form, and comments
   * to output valid HTML5.
-  */
+  **/
   add_theme_support( 'html5', array(
       'search-form',
       'comment-form',
