cdd6842d03d6b53fe3e7efa59cd3be979b4c0d5d
Ben Brehaut <bencbrehaut@gmail.com>
2017-04-03 22:54:45 +0100

    Unenqued the main contact form 7 stylesheet
    This closes #11

--- lib/enqueue-scripts.php
+++ lib/enqueue-scripts.php
@@ -15,29 +15,29 @@
 * Add main CSS and JS files
 **/
 function theme_scripts() {
-    // Enqueue the main Stylesheet.
 
-    $theme_css = get_template_directory() . '/assets/css/style.css';
+  // Enqueue the main Stylesheet.
+  $theme_css = get_template_directory() . '/assets/css/style.css';
 
-    wp_enqueue_style( 'main-stylesheet', get_template_directory_uri() . '/assets/css/style.css', array(), filemtime( $theme_css ), 'all' );
+  wp_enqueue_style( 'main-stylesheet', get_template_directory_uri() . '/assets/css/style.css', array(), filemtime( $theme_css ), 'all' );
 
-    // Deregister the jquery version bundled with WordPress, as it does get old.
-    wp_deregister_script( 'jquery' );
+  // Deregister the jquery version bundled with WordPress, as it does get old.
+  wp_deregister_script( 'jquery' );
 
-    // CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
-    wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js', array(), '1.0.0', true );
+  // CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
+  wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js', array(), '1.0.0', true );
 
-    // Enqueue the main javascript file.
-    // By default its the uncompressed version, but you can easily change this
+  // Enqueue the main javascript file.
+  // By default its the uncompressed version, but you can easily change this
 
-    $theme_js = get_template_directory() . '/assets/js/scripts.js';
+  $theme_js = get_template_directory() . '/assets/js/scripts.js';
 
-    wp_enqueue_script( 'main-scripts', get_template_directory_uri() . '/assets/js/scripts.js', array('jquery'), filemtime( $theme_js ), true );
+  wp_enqueue_script( 'main-scripts', get_template_directory_uri() . '/assets/js/scripts.js', array('jquery'), filemtime( $theme_js ), true );
 
-    // Add the comment-reply library on pages where it is necessary
-    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-        wp_enqueue_script( 'comment-reply' );
-    }
+  // Add the comment-reply library on pages where it is necessary
+  if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+    wp_enqueue_script( 'comment-reply' );
+  }
 }
 add_action( 'wp_enqueue_scripts', 'theme_scripts' );
 
@@ -60,6 +60,14 @@ function add_ie_html5_shim () {
   echo '<![endif]-->';
 }
 add_action('wp_head', 'add_ie_html5_shim');
+
+/**
+* Removes Contact form 7 main stylesheet
+**/
+function contact_form_styles() {
+  wp_deregister_style( 'contact-form-7' );
+}
+add_action( 'wp_print_styles', 'contact_form_styles', 100 );
 
 /**
 * Add Favicons to site and also admin area
