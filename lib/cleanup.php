28c1af65b3f97698b9a2d3f16cfae1fe277f4d11
Ben Brehaut <bencbrehaut@gmail.com>
2017-04-25 18:29:09 +0100

    Fixing PHP warning issue

--- lib/cleanup.php
+++ lib/cleanup.php
@@ -92,7 +92,7 @@ function add_current_nav_class($classes, $item) {
   global $post;
   // Getting the post type of the current post
   $current_post_type = get_post_type_object(get_post_type($post->ID));
-  $current_post_type_slug = $current_post_type->rewrite[slug];
+  $current_post_type_slug = $current_post_type->rewrite['slug'];
   // Getting the URL of the menu item
   $menu_slug = strtolower(trim($item->url));
   // If the menu item URL contains the current post types slug add the current-menu-item class
